0322da782916fc9a946ccbba436987ecc806abd8
Prithu Singh Kathayat <40120888+Prithuk@users.noreply.github.com>
2022-07-28 10:45:11 +0545

    Update Dbutil.java

--- src/main/java/com/prithu/util/Dbutil.java
+++ src/main/java/com/prithu/util/Dbutil.java
@@ -12,7 +12,7 @@ public class Dbutil {
 	private static final String URL ="jdbc:mysql://localhost:3306/";
 	private static final String DB_NAME ="forservlet";
 	private static final String USER_NAME ="root";
-	private static final String PASSWORD ="kathet28don";
+	private static final String PASSWORD ="root";
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName(DRIVER_NAME);
